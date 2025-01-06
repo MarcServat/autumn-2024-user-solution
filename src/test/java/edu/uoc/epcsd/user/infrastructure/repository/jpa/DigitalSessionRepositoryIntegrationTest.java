29fb310ab26bbf4294f2fd13d1863dd8eca0d0bb
Marc Servat <marcsrvt@gmail.com>
2025-01-06 19:08:05 +0100

    Exercise 2 completed

--- src/test/java/edu/uoc/epcsd/user/infrastructure/repository/jpa/DigitalSessionRepositoryIntegrationTest.java
+++ src/test/java/edu/uoc/epcsd/user/infrastructure/repository/jpa/DigitalSessionRepositoryIntegrationTest.java
@@ -1,40 +1,35 @@
 package edu.uoc.epcsd.user.infrastructure.repository.jpa;
 
+import edu.uoc.epcsd.user.UserApplication;
 import edu.uoc.epcsd.user.domain.DigitalSession;
 import edu.uoc.epcsd.user.domain.repository.DigitalSessionRepository;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Replace with an embedded database
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = UserApplication.class)
 public class DigitalSessionRepositoryIntegrationTest {
 
     @Autowired
     private DigitalSessionRepository digitalSessionRepository;
 
-    @MockBean
-    private SpringDataDigitalSessionRepository jpaDigitalSessionRepository;
+    @Autowired
+    private SpringDataDigitalSessionRepository springDataDigitalSessionRepository;
 
     @Autowired
-    private TestEntityManager entityManager;
+    private SpringDataUserRepository springDataUserRepository;
 
     @Test
     void whenAddingDigitalSession_thenFindDigitalSessionByUserId() {
         // Arrange
         UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
         userEntity.setFullName("John Doe");
         userEntity.setEmail("john.doe@example.com");
         userEntity.setPassword("password");
@@ -45,15 +40,13 @@ public class DigitalSessionRepositoryIntegrationTest {
         digitalSessionEntity.setDescription("description");
         digitalSessionEntity.setLocation("location");
         digitalSessionEntity.setLink("link");
+
+        springDataUserRepository.save(userEntity);
         digitalSessionEntity.setUser(userEntity);
-
-        entityManager.persist(userEntity);
-        entityManager.persist(digitalSessionEntity);
-
-        when(jpaDigitalSessionRepository.findDigitalSessionByUser(userEntity.getId())).thenReturn(List.of(digitalSessionEntity));
+        Long userId = springDataDigitalSessionRepository.save(digitalSessionEntity).getUser().getId();
 
         // Act
-        List<DigitalSession> sessions = digitalSessionRepository.findDigitalSessionByUser(userEntity.getId());
+        List<DigitalSession> sessions = digitalSessionRepository.findDigitalSessionByUser(userId);
 
         // Assert
         assertThat(sessions).hasSize(1);
@@ -61,6 +54,7 @@ public class DigitalSessionRepositoryIntegrationTest {
         assertThat(sessions.get(0).getDescription()).isEqualTo(digitalSessionEntity.getDescription());
         assertThat(sessions.get(0).getLocation()).isEqualTo(digitalSessionEntity.getLocation());
         assertThat(sessions.get(0).getLink()).isEqualTo(digitalSessionEntity.getLink());
-        assertThat(sessions.get(0).getUserId()).isEqualTo(digitalSessionEntity.getUser().getId());    }
+        assertThat(sessions.get(0).getUserId()).isEqualTo(digitalSessionEntity.getUser().getId());
+    }
 
 }
